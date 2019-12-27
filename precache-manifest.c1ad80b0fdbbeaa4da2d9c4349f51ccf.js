dd289d8b5e34da417ca0e8143ea056455a59a9c3
Kartik Sahoo <kartik.mca09@gmail.com>
2019-12-27 22:36:53 +0530

    Updates

---
+++ precache-manifest.c1ad80b0fdbbeaa4da2d9c4349f51ccf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3f45dde02b35a38a9c335548e2be8d3",
+    "url": "/table-renderer/index.html"
+  },
+  {
+    "revision": "19d9d003fa9dba6c0d53",
+    "url": "/table-renderer/static/css/main.2e56f084.chunk.css"
+  },
+  {
+    "revision": "b66e676a26d0bd207a00",
+    "url": "/table-renderer/static/js/2.f0c74d83.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/table-renderer/static/js/2.f0c74d83.chunk.js.LICENSE"
+  },
+  {
+    "revision": "19d9d003fa9dba6c0d53",
+    "url": "/table-renderer/static/js/main.0c16fef4.chunk.js"
+  },
+  {
+    "revision": "3490c3ef756d2dce1f70",
+    "url": "/table-renderer/static/js/runtime-main.69a921b0.js"
+  }
+]);
